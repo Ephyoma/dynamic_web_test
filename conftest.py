fac65f187decaef8c744da1e1edd4c65715c12ba
Ephyoma <ifyomaokorie@gmail.com>
2025-05-13 13:44:56 +0100

    Fix indentation error in conftest.py

--- conftest.py
+++ conftest.py
@@ -6,14 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
+    chrome_options = Options()
     chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-    
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.maximize_window()
     yield driver
     driver.quit()
